9faa31a882bb1c97609276bc62f8fa0d638541df
lan-ti <473992707@qq.com>
2020-06-20 17:48:54 +0800

    6月20,添加Demo3,完成功能：把a.txt文件里的内容复制到b.txt

---
+++ src/cn/Demo3.java
@@ -0,0 +1,21 @@
+package cn;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Demo3 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new FileReader("a.txt"));
+        BufferedWriter bw = new BufferedWriter(new FileWriter("b.txt"));
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            bw.write(line);
+            bw.newLine();
+            bw.flush();
+        }
+        br.close();
+        bw.close();
+    }
+}
